e714e0a03da737238d9f3a81ad99e0329641bc12
Philisizwe <cre8storeese@gmail.com>
2023-02-16 10:51:09 -0800

    Correction to 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - the function
  * {} - the code being executed
  * ; - to complete the code
- */ 
+ * Return: 0 - success
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
