d4f90364132e7f1704211c12826062c75d93c04d
Philisizwe <cre8storeese@gmail.com>
2023-02-16 11:14:15 -0800

    Answer to 6. Size is not grandeur, and terrioty does does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the function
+ * {} - the code being executed
+ * ; - to complete the code
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int a;
+	double b;
+	char c;
+	float d;
+
+	printf("Size of an int: %lu byte(s)\n, sizeof(a)");
+	printf("Size of a double: %lu byte(s)\n, sizeof(b)");
+	printf("Size of a char: %lu byte(s)\n, sizeof(c)");
+	printf("Size of a float: %lu byte(s)\n, sizeof(d)");
+
+	return (0);
+}
