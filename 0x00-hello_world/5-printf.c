3a78ffca6b688bac25e4d1fa1164525543cf1265
Philisizwe <cre8storeese@gmail.com>
2023-02-16 10:58:10 -0800

    Answer to 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the function
+ * {} - the code being executed
+ * ; - to complete the code
+ * Return: 0 - success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
