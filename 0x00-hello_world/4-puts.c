b2d3d5b3f1cbe7aef2bbb28f14016bf150e7c217
Philisizwe <cre8storeese@gmail.com>
2023-02-16 10:44:46 -0800

    Answer to 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the function
+ * {} - the code being executed
+ * ; - to complete the code
+ */ 
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
