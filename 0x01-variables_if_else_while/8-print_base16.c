88ed6a986b5098cb781a47ae7388039479c7c9f9
Philisizwe <cre8storeese@gmail.com>
2023-02-17 14:10:14 -0800

    Answer to 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting function
+ * {} - executed code 
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	putchar(num);
+	for (num = 'a'; num <= 'f'; num++)
+	putchar(num);
+	putchar('\n');
+
+	return (0);
+}
