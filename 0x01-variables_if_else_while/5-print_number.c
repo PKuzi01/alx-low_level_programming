8744de3467835c648763d3f6eff9097a6abb98f6
Philisizwe <cre8storeese@gmail.com>
2023-02-17 13:33:19 -0800

    Answer to 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - starting function
+ * {} - executed code
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	printf(%i, num);
+	putchar('\n');
+
+	return (0);
+}
