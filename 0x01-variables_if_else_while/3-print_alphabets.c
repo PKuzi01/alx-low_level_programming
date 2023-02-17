03f44ce32b914c766513f1c6cc253cc8a16c3759
Philisizwe <cre8storeese@gmail.com>
2023-02-17 12:58:06 -0800

    Answer to 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - starting function
+ * {} - executed code, nested codeblocks
+ * Return: 0 - success
+ */
+
+int main(void)
+{
+       	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+       	putchar('\n');
+
+	return (0);
+}
+ 
