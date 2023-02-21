7592b0031ba4e55a9bc2f68616383c71ca9d5713
Philisizwe <cre8storeese@gmail.com>
2023-02-21 13:23:45 -0800

    Correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (letter = 'a'; letter <= "z"; letter++)
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 			_putchar('\n');
