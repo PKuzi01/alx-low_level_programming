9e02ad660caab6cc21f93695d2e666c65638ba28
Philisizwe <cre8storeese@gmail.com>
2023-02-21 13:04:49 -0800

    Correction to 0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,15 +8,13 @@
 
 int main(void)
 {
-	int i;
-	char x[] = "_putchar";
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(x[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
-	return (0);
 }
