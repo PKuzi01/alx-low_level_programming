62dc478189df2a1d69ba6515be0ba559036421f9
Philisizwe <cre8storeese@gmail.com>
2023-02-21 12:09:30 -0800

    Answer to 0. _putchar

--- 0x02-functions_nested_loops/0-_putchar.c
+++ 0x02-functions_nested_loops/0-_putchar.c
@@ -1,20 +1,22 @@
 #include "main.h"
 
 /**
- * main - print _putchar, followed by a new line
+ * main - stating function
+ * {} - executed code
  * Return: 0 - success
  */
 
 int main(void)
 {
 	int i;
-       	char x[] = "_putchar";
-	
+	char x[] = "_putchar";
+
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(x[i]);
-        }
+	}
 
 	_putchar('\n');
+	
 	return (0);
 }
