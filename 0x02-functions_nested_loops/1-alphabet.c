53594eb1ca2df11fb47c38f27bb32c8ed42fde61
Philisizwe <cre8storeese@gmail.com>
2023-02-21 12:29:41 -0800

    Answer to 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet: print the alphabet in lowercase
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int letter;
+
+	for (letter = 97, letter < 123; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+}
