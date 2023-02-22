c985c78820de13eeff8af0f12be070bf7085e87e
Philisizwe <cre8storeese@gmail.com>
2023-02-22 08:07:36 -0800

    Doc and coding style change

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * times_table: print the 9 times table, starting with 0
+ * times_table - print the 9 times table, starting with 0
+ *
  * Return: 0 - success
 */
 
@@ -12,13 +13,13 @@ void times_table(void)
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
-		
+
 		for (b = 1; b <= 9; b++)
 		{
 			c = a * b;
 			_putchar(44);
 			_putchar(32);
-			
+
 			if (c <= 9)
 			{
 				_putchar(32);
@@ -29,7 +30,7 @@ void times_table(void)
 				_putchar((c % 10) + 48);
 			}
 		}
-	
+
 	_putchar('\n');
 	}
 }
