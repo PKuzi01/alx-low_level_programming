601700841e8aa949767fa40e3dda6d042ef79967
Philisizwe <cre8storeese@gmail.com>
2023-02-22 07:53:48 -0800

    Doc and coding style change

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign- prints the sign of a number
+ * print_sign - prints the sign of a number
  * (greater than less than or equal to)
- * return- 1 and prints + if n > 0,
+ *
+ * @n: charcter in ASCII table
+ *
+ * Return: 1 and prints + if n > 0,
  * 0 and prints 0 if n = 0,
  * -1 and prints - n > 0
 */
@@ -11,18 +14,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		{
-			_putchar(43);
-			return (1);
-		} else if (n < 0)
-		{
-			_putchar(45);
-			return (-1);
-		} else
-		{
-			_putchar(48);
-			return (0);
-		}
-
-		_putchar('\n');
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n');
 }
