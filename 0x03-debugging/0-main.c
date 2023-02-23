99dd16b982ff3730f98d47acf8e861fd976eb60d
Philisizwe <cre8storeese@gmail.com>
2023-02-23 12:59:08 -0800

    Answer for 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function print if int is neg or pos
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int x;
+
+	x = 0;
+	positive_or_negative(x);
+
+	return (0);
+}
