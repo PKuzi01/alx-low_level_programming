b4fa7e9c1ef3316ba0af39ab63abf4ebfe600f77
Philisizwe <cre8storeese@gmail.com>
2023-02-23 13:56:55 -0800

    Correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,10 +14,13 @@ void print_most_numbers(void)
 
 	for (x = 0; x < 9; x++)
 	{
-		if (x != 2 || x != 4)
+		if (x != 2)
 		{
-			_putchar(x + '0');
+			if (x != 4)
+			{
+				_putchar(x + '0');
+			}
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
