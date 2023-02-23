cb776551a263add3f4ab8b4d0f455711e3cc4a51
Philisizwe <cre8storeese@gmail.com>
2023-02-23 10:15:34 -0800

    Answer to 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *
+ * @n: number of times '_' is printed
+ *
+ * Return: 0 (success)
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('_');
+		}
+	}
+}
+
