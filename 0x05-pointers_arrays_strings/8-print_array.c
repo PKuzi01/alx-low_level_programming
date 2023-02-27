b4ca2900359581e762293e1130bbc7f82e27c06e
Philisizwe <cre8storeese@gmail.com>
2023-02-27 07:56:59 -0800

    Correction to 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of an array of integers,
@@ -14,7 +15,7 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0; x < n; i++)
+	for (x = 0; x < n; x++)
 	{
 		if (x < n - 1)
 		{
