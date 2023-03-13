7bca817f5673ca8d234f13ee9a347579b7145cf0
Philisizwe <cre8storeese@gmail.com>
2023-03-13 01:49:18 -0700

    Answer to 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - count the length of a string
+ *
+ * @s: pointer
+ *
+ * Return: x
+ */
+
+unsigned int _strlen(char *s)
+{
+	unsigned int x;
+
+	for (c = 0; s[c]; c++)
+		;
+
+	return (c);
+}
+
+/**
+ * _strdup - create a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter
+ *
+ * @str: pointer
+ *
+ * Return: dup
+ */
+char *_strdup(char *str)
+{
+	unsigned int x, size;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	size = _strlen(str);
+	dup = malloc(sizeof(char) * (size + 1));
+
+	if (dup == NULL)
+		return (NULL);
+
+	for (x = 0; str[x]; x++)
+		dup[x] = str[x];
+
+	return (dup);
+}
